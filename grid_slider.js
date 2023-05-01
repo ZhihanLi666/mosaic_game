a663f3a8a3ed29b29695dcef381e74c799c4f4bd
ZhihanLi666 <60561882+ZhihanLi666@users.noreply.github.com>
2023-05-01 16:05:58 -0400

    Update grid_slider.js

--- grid_slider.js
+++ grid_slider.js
@@ -10,76 +10,78 @@ let initialPositions = [];
 // Store initial positions of images
 images.forEach((image) => {
     const position = {
-      left: image.offsetLeft,
-      top: image.offsetTop
+        left: image.offsetLeft,
+        top: image.offsetTop
     };
     initialPositions.push(position);
-  });
-  function resetImages() {
+});
+
+function resetImages() {
     images.forEach((img, index) => {
-      img.style.left = initialPositions[index].left;
-      img.style.top = initialPositions[index].top;
+        img.style.left = initialPositions[index].left;
+        img.style.top = initialPositions[index].top;
     });
-  }
+}
 
 container.addEventListener('mousedown', dragStart);
+container.addEventListener('touchstart', dragStart);
 container.addEventListener('mousemove', drag);
+container.addEventListener('touchmove', drag);
 container.addEventListener('mouseup', dragEnd);
-
+container.addEventListener('touchend', dragEnd);
 
 document.querySelector('#ss1').addEventListener('click', function() {
-  Image(1);
-  resetImages();
+    Image(1);
+    resetImages();
 });
 document.querySelector('#ds1').addEventListener('click', function() {
-  Image(0);
-  resetImages();
+    Image(0);
+    resetImages();
 });
 
-
-
 function dragStart(event) {
-  if (event.target.classList.contains('drag')) {
-    dragItem = event.target;
-    dragOffsetX = event.clientX - dragItem.offsetLeft;
-    dragOffsetY = event.clientY - dragItem.offsetTop;
-    i++;
-    dragItem.style.zIndex=i
-  }
+    if (event.target.classList.contains('drag')) {
+        event.preventDefault();
+        dragItem = event.target;
+        if (event.type === 'touchstart') {
+            dragOffsetX = event.touches[0].clientX - dragItem.offsetLeft;
+            dragOffsetY = event.touches[0].clientY - dragItem.offsetTop;
+        } else {
+            dragOffsetX = event.clientX - dragItem.offsetLeft;
+            dragOffsetY = event.clientY - dragItem.offsetTop;
+        }
+        i++;
+        dragItem.style.zIndex=i
+    }
 }
 
 function drag(event) {
-  if (dragItem !== null) {
-    event.preventDefault();
-    
-    dragItem.style.left = (event.clientX - dragOffsetX) + 'px';
-    dragItem.style.top = (event.clientY - dragOffsetY) + 'px';
-  }
+    if (dragItem !== null) {
+        event.preventDefault();
+        if (event.type === 'touchmove') {
+            dragItem.style.left = (event.touches[0].clientX - dragOffsetX) + 'px';
+            dragItem.style.top = (event.touches[0].clientY - dragOffsetY) + 'px';
+        } else {
+            dragItem.style.left = (event.clientX - dragOffsetX) + 'px';
+            dragItem.style.top = (event.clientY - dragOffsetY) + 'px';
+        }
+    }
 }
 
 function dragEnd(event) {
-  if (dragItem !== null) {
-    i++;
-    dragItem.style.zIndex=i;
-    
-    dragItem = null;
-  }
+    if (dragItem !== null) {
+        i++;
+        dragItem.style.zIndex=i;
+        dragItem = null;
+    }
 }
 
 function Image(n) {
-  for (let j = 1; j <= 9; j++) {
-
-    if (`jigsaw/${n*9+j}.png` !== null) {
-      document.querySelector(`#image${j}`).src = `jigsaw/${n*9+j}.png`;
-    // Do something with the image
-  } 
-      
-    else {
-      document.querySelector(`#image${j}`).src = `00.png`;
+    for (let j = 1; j <= 9; j++) {
+        if (`jigsaw/${n*9+j}.png` !== null) {
+            document.querySelector(`#image${j}`).src = `jigsaw/${n*9+j}.png`;
+        } else {
+            document.querySelector(`#image${j}`).src = `00.png`;
+        }
     }
-      
-
-  
 }
-}
-
